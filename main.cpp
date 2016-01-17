48c0197eb0438a5ee8c517adfdd9ae6e778d1075
Christopher Gleeson <christopherkgleeson@gmail.com>
2016-01-17 01:49:56 -0600

    Refactor
    Changed tests to reflect the refactor in BST.hpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 //  main.cpp
 //  C++ClassBST
 //
-//  Tester for the BST class implemented in BST.hpp.
+//  Basic BST class implementation with insert, search, remove, recursive inorder print,
+//  and iterative inorder print.
 //
+//  The data type of value in the leaf struct is templateized.
 //
 //  Created by Christopher Gleeson on 12/21/15.
 //  Copyright © 2015 Christopher Gleeson. All rights reserved.
@@ -74,16 +76,42 @@ int main(int argc, const char * argv[]){
     myTree->inorder_predecessor(2);
     myTree->inorder_predecessor(12);
     myTree->inorder_predecessor(14);
-    
-    //test find parent
-    std::cout << "Testing find parent for node with value 4, expect to find parent with value 8.\n";
-    myTree->locate_parent(4);
-    
+        
     //test deletion
-    std::cout << "Testing node deletion of node with value 14, expect success.\n";
-    myTree->remove(14);
+    int del = 14;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
     myTree->print_tree_inorder();
-
+    del = 4;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = 2;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = -16;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = -1;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = 10;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = 5;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    del = 3;
+    std::cout << "Testing node deletion of node with value " << del << ", expect success.\n";
+    myTree->remove(del);
+    myTree->print_tree_inorder();
+    
+    
     std::cout << "Destroying tree.\n";
     delete myTree;
     return 0;
